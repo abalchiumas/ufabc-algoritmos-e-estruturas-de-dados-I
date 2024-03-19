bffc36e972bdf098f62a4a91e481c1700cfb4a8c
Andre Balchiumas <a.balchiumas@gmail.com>
2024-03-18 23:52:36 -0300

    Resolução quase completa do exercício de balizamento

--- src/unidade_03/35_balizamento.c
+++ src/unidade_03/35_balizamento.c
@@ -6,70 +6,84 @@ typedef struct tAtleta {
 	int centesimosTotal;
 } Atleta;
 
-// insertion sort baseado nos centesimos totais de cada atleta
 void reordenarAtletas(Atleta *atletas, const int qntdAtletas) {
 	for (int j = 1; j < qntdAtletas; j++) {
-		Atleta key = atletas[j];
+		Atleta chave = atletas[j];
 		int i = j - 1;
-		while ((i >= 0) && (atletas[i].centesimosTotal > key.centesimosTotal)) {
-			atletas[i + 1] = atletas[i];
+		while ((i >= 0) && (atletas[i].centesimosTotal > chave.centesimosTotal)) {
+			atletas[i + 1] = atletas[i];	
 			i--;
 		}
-		atletas[i + 1] = key;
+		atletas[i + 1] = chave;
 	}
 }
 
-void balizamento(Atleta *atletas, const int qntdRaias) {
-	printf("\n"); // TODO: remove later
-	// Atleta *atletasCopy = malloc(m * sizeof(Atleta));
-	// for (int k = 0; k < m; k++) { // cria uma cópia do ponteiro recebido
-	// 	atletasCopy[k] = atletas[k];
-	// }
+Atleta *mergeAtletas(Atleta *atletasComBalizamento, const int qntdAtletasComBalizamento, Atleta *atletasSemBalizamento, const int qntdAtletasSemBalizamento) {
+	Atleta *atletasMerge = malloc((qntdAtletasComBalizamento + qntdAtletasSemBalizamento) * sizeof(Atleta));
+	for (int i = 0; i < qntdAtletasComBalizamento; i++) {
+        atletasMerge[i] = atletasComBalizamento[i];
+    }
+    for (int i = 0; i < qntdAtletasSemBalizamento; i++) {
+        atletasMerge[qntdAtletasComBalizamento + i] = atletasSemBalizamento[i];
+    }
+	free(atletasComBalizamento);
+	free(atletasSemBalizamento);
+	return atletasMerge;	
+}
 
-	int *posicoes = malloc(qntdRaias * sizeof(int));
-	int posicao = (qntdRaias - 1) / 2;
-	int modificador = 1;
-	for (int i = 0; i < qntdRaias; i++) {
-		posicoes[posicao] = i + 1;
-		posicao = posicao + (modificador * (i + 1));
-		modificador = modificador * -1;
-	}
-
-	for (int j = 0; j < qntdRaias; j++) {
-		printf("%d ", posicoes[j]); // TODO: remove later
+void balizamento(Atleta *atletas, const int qntdSeries, const int qntdRaias, const int qntdAtletas) {
+	int endIndex = qntdAtletas, startIndex = (qntdAtletas - (1 - (qntdAtletas % 2)) - (qntdAtletas % qntdRaias)); // funciona para (qntdRaias, qntdAtletas) = (8, 10), (7, 11), (8, 18), (8, 5)
+	for (int serie = 0; serie < qntdSeries; serie++) {
+		printf("%da. serie:\n", serie + 1);
+		int raiaAtletaInicial = (qntdRaias + (qntdRaias % 2)) / 2, modificadorNumeroRaia = 1, contadorRaia = 1;
+		for (int k = startIndex; k < endIndex; k++) {
+			printf("Raia %d: %s\n", raiaAtletaInicial, atletas[k].nome);
+			raiaAtletaInicial = raiaAtletaInicial + (modificadorNumeroRaia * contadorRaia);
+			modificadorNumeroRaia = modificadorNumeroRaia * -1;
+			contadorRaia++;
+		}
+		endIndex = startIndex;
+		startIndex = startIndex - qntdRaias + (1 - (qntdAtletas % 2)); 
 	}	
-	printf("\n\n");
 }
 
 int main(void) {
-	int qntdRaias, qntdAtletas;
+	int qntdRaias, qntdAtletas, counter, qntdAtletasSemBalizamento, qntdAtletasComBalizamento;;
 	scanf("%d", &qntdRaias);
 	scanf("%d", &qntdAtletas);
-	Atleta *atletas = NULL;
-	for (int counter = 0; counter < qntdAtletas; counter++) {
+	Atleta *atletasComBalizamento = NULL, *atletasSemBalizamento = NULL;
+	while (counter < qntdAtletas) {
 		Atleta atletaTmp;
 		atletaTmp.nome = malloc(100 * sizeof(char));
-		atletas = realloc(atletas, (counter + 1) * sizeof(Atleta));
+
 		int min, seg, cent;
 		scanf("%s %d %d %d", atletaTmp.nome, &min, &seg, &cent);
+		
 		atletaTmp.centesimosTotal = (((min * 60) + seg) * 100) + cent;
-		atletas[counter] = atletaTmp;
+		if (atletaTmp.centesimosTotal == 0) {
+			atletasSemBalizamento = realloc(atletasSemBalizamento, (qntdAtletasSemBalizamento + 1) * sizeof(Atleta));
+			atletasSemBalizamento[qntdAtletasSemBalizamento++] = atletaTmp;
+		} else {
+			atletasComBalizamento = realloc(atletasComBalizamento, (qntdAtletasComBalizamento + 1) * sizeof(Atleta));
+			atletasComBalizamento[qntdAtletasComBalizamento++] = atletaTmp;
+		}
+		counter++;
 	}
+	reordenarAtletas(atletasComBalizamento, qntdAtletasComBalizamento);
+	Atleta *atletas = mergeAtletas(atletasComBalizamento, qntdAtletasComBalizamento, atletasSemBalizamento, qntdAtletasSemBalizamento);
 
-	int qntdSeries = ((qntdAtletas / qntdRaias) == 0) ? (qntdAtletas / qntdRaias) : (qntdAtletas / qntdRaias) + 1;
+	int qntdSeries = 1;
+	if (qntdRaias < qntdAtletas) { qntdSeries = (qntdAtletas % qntdRaias) == 0 ? (qntdAtletas / qntdRaias) : (qntdAtletas / qntdRaias) + 1; }	
 	if (qntdSeries == 1) { printf("\n%d serie\n", qntdSeries); } 
 	else { printf("\n%d series\n", qntdSeries); }
 
-	reordenarAtletas(atletas, qntdAtletas);
+	balizamento(atletas, qntdSeries, qntdRaias, qntdAtletas);
 
-	// balizamento(atletas, qntdAtletas);
-
+	printf("\nLista ordenada pelo tempo de balizamento:\n");
 	for (int j = 0; j < qntdAtletas; j++) {
 		printf("%d) %s | %d\n", j, atletas[j].nome, atletas[j].centesimosTotal);
 		free(atletas[j].nome);
 	}	
-	free(atletas);
-	
-	
+	free(atletas);	
 	return 0;
 }
