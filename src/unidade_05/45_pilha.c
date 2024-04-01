1fcf6c753671bf6badc51193f81f35c48bc23ccf
Andre Balchiumas <a.balchiumas@gmail.com>
2024-03-31 21:36:20 -0300

    Resolução do ex 45 e implementação inicial do ex 46

--- src/unidade_05/45_pilha.c
+++ src/unidade_05/45_pilha.c
@@ -15,37 +15,54 @@ typedef struct tPilha {
 // Devolve o ponteiro para a pilha alocada, com os campos inicializados:
 // topo vazio e quantidade zero.
 Pilha *criaPilhaVazia() {
-
+    Pilha *novaPilha = malloc(sizeof(Pilha));
+    novaPilha->topo = NULL;
+    novaPilha->quantidade = 0;
+    return novaPilha;
 }
 
 // Recebe um inteiro x.
 // Aloca memoria para criar um novo item.
 // Inicializa o valor da chave com x e o ponteiro prox com vazio.
-Item *criaItem(int chave) {
-
+Item *criaItem(const int chave) {
+    Item *novoItem = malloc(sizeof(Item));
+    novoItem->chave = chave;
+    novoItem->proximoItem = NULL;
+    return novoItem;
 }
 
 // Deve-se liberar primeiro a cadeia de itens.
 // Depois, no final, liberar o ponteiro da pilha.
 void liberaPilha(Pilha *pilha) {
-
+    Item *pilhaTopo = pilha->topo;
+	while (pilhaTopo != NULL) {
+		Item *itemTemporario = pilhaTopo;
+        pilhaTopo = pilhaTopo->proximoItem;
+        free(itemTemporario);
+	}
+	free(pilha);
 }
 
 // Recebe um ponteiro para uma pilha.
 // Devolve 1 se pilha vazia, 0 caso contrario.
-int vazia(Pilha *pilha) {
-
-}
+int vazia(const Pilha *pilha) { return pilha->topo == NULL; }
 
 // Imprime o conteudo da pilha, do topo para a base.
-void imprimir(Pilha *pilha) {
-
+void imprimir(const Pilha *pilha) {
+    Item *itemAtual = pilha->topo;
+    while (itemAtual != NULL) {
+        printf("%d ", itemAtual->chave);
+        itemAtual = itemAtual->proximoItem;
+    }
+    printf("\n");
 }
 
 // Recebe um ponteiro para uma pilha e uma chave x.
 // Insere x no topo da pilha.
-void empilha(Pilha *pilha, int chave) {
-
+void empilha(Pilha *pilha, const int chave) {
+    Item *novoItem = criaItem(chave);
+    novoItem->proximoItem = pilha->topo;
+    pilha->topo = novoItem;
 }
 
 // Recebe um ponteiro para uma pilha.
@@ -53,10 +70,35 @@ void empilha(Pilha *pilha, int chave) {
 // Devolve a chave removida.
 // Devolve -1 se pilha vazia.
 int desempilha(Pilha *pilha) {
-
+    if (vazia(pilha)) { return -1; }
+    else {
+        Item *topoAntigo = pilha->topo;
+        int chaveParaRemover = topoAntigo->chave;
+        pilha->topo = topoAntigo->proximoItem;
+        free(topoAntigo);
+        printf("[ %d ]\n", chaveParaRemover);
+        return chaveParaRemover;
+    }   
 }
 
 int main(void) {
-	printf("Hello!");
+	Pilha *pilha = criaPilhaVazia();
+    char operacao;
+    int chave;
+    while(scanf(" %c", &operacao) != EOF) {
+        switch (operacao) {
+            case 'E':
+                scanf(" %d", &chave);
+                empilha(pilha, chave);
+                break;
+            case 'D':
+                desempilha(pilha);
+                break;
+            case 'M':
+                imprimir(pilha);
+                break;
+        }
+    }
+    liberaPilha(pilha);
 	return 0;
 }
--- src/unidade_05/46_fila.c
+++ src/unidade_05/46_fila.c
@@ -1,6 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct tItem {
+    int chave;
+    struct tItem *proximoItem;  // ponteiro para proximo na lista
+} Item;
+
+typedef struct tFila {
+    Item *inicio, *fim;   // ponteiro para o inicio e fim da fila
+} Fila;
+
+// Aloca memoria para criar uma fila inicialmente vazia.
+// Devolve o ponteiro para a fila alocada, com os campos inicializados:
+// inicio e fim vazios.
+Fila *criaFilaVazia() {
+	Fila *novaFila = malloc(sizeof(Fila));
+	novaFila->inicio = NULL, novaFila->fim = NULL;
+	return novaFila;
+}
+
+// Recebe um inteiro x.
+// Aloca memoria para criar um novo item.
+// Inicializa o valor da chave com x e o ponteiro prox com vazio.
+Item *criaItem(const int chave) {
+	Item *novoItem = malloc(sizeof(Item));
+	novoItem->chave = chave;
+	novoItem->proximoItem = NULL;
+	return novoItem;
+}
+
+// Deve-se liberar primeiro a cadeia de itens.
+// Depois, no final, liberar o ponteiro da fila.
+void liberaFila(Fila *fila) {
+	
+}
+
+// Recebe um ponteiro para uma fila.
+// Devolve 1 se fila vazia, 0 caso contrario.
+int vazia(const Fila *fila) {
+	
+}
+
+// Imprime o conteudo da fila, do topo para a base.
+void imprimir(const Fila *fila) {
+	
+}
+
+// Recebe um ponteiro para uma fila e uma chave x.
+// Insere x no fim da fila.
+void enfileirar(Fila *fila, const int chave) {
+	
+}
+
+// Recebe um ponteiro para uma fila.
+// Se fila nao vazia, remove elemento do inicio da fila.
+// Devolve a chave removida.
+// Devolve -1 se fila vazia.
+int desenfileirar(Fila *fila) {
+	
+}
+
 int main(void) {
 	printf("Hello!");
 	return 0;
